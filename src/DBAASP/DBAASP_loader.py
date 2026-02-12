0d0da68a5c26f1718922f795ee609715fbd1f345
Dominika Šenová <domisenova@seznam.cz>
2026-02-12 17:57:52 +0100

    add DBAASP raw data

---
+++ src/DBAASP/DBAASP_loader.py
@@ -0,0 +1,17 @@
+"""
+Filter raw data from DBAASP,
+create positive and negative dataset.
+"""
+
+
+# --- SETTINGS ---
+bsubtilis = "data_DBAASP/raw/DBAASP_bsubtilis_peptides.csv"
+ecoli = "data_DBAASP/raw/DBAASP_ecoli_peptides.csv"
+paegiurosa = "data_DBAASP/raw/DBAASP_paegiurosa_peptides.csv"
+saureus = "data_DBAASP/raw/DBAASP_saureus_peptides.csv"
+sepidermidis = "data_DBAASP/raw/DBAASP_sepidermidis_peptides.csv"
+
+threshold_active = 25
+threshold_inactive = 75
+# ----------------
+
