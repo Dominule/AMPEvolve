69bc422a37fb5cfd2b95a160eaaa967a8b7afb52
Dominika Šenová <domisenova@seznam.cz>
2026-02-13 16:28:37 +0100

    update analyzer.py

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,18 +3,18 @@ Combines calculations from calculator.py and predictor.py,
 generates pd.DataFrame with seqs and additional information relevant for AMPs.
 """
 
+import pandas as pd
 import calculator
 import predictor
 
-def analyze_AMPs(seqs: list[str], names=[]) -> pd.DataFrame:
-    if not names:
-        names = range(0, len(seqs))
-
+def analyze_AMPs(seqs: list[str], storage_path) -> pd.DataFrame:
     df = calculator.amps_analysis(seqs)
 
     macrel = predictor.MacrelPredictor()
     predictions = macrel.calculate_and_predict_seqs(seqs)
 
     df["Macrel"] = predictions
+    df.sort_values(by="Macrel", ascending=False, inplace=True)
+    df.to_csv(storage_path, index=True)
 
     return df
